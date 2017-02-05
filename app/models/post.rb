df579fcfa38cf1c8df48c0276ed637b054d09e34
Pawan Sunuwar <pawanman56@gmail.com>
2017-02-05 13:50:54 +0000

    Write post backend

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
     
     mount_uploader :photo, PhotoUploader
     
+    belongs_to :user
+    
+    validates :photo, :description, :user_id, presence: true
+    
 end
