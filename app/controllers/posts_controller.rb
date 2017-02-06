f5af7c1f02361f3eeaeb526ab36900c3aaf130dc
Pawan Sunuwar <pawanman56@gmail.com>
2017-02-06 08:20:01 +0000

    Delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
     
     before_action :authenticate_user!, only: [:new, :create]
-    before_action :is_owner?, only: [:edit, :update]
+    before_action :is_owner?, only: [:edit, :update, :destroy]
     
     def index
         @posts = Post.order('id DESC')
@@ -33,6 +33,12 @@ class PostsController < ApplicationController
         else
             render :edit, status: :unprocessable_entity
         end
+    end
+    
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to root_path
     end
     
     private
