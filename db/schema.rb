44746cb18e24ec901d859f20bfcb2c09b2a5ffd7
Pawan Sunuwar <pawanman56@gmail.com>
2017-02-06 08:59:34 +0000

    Create comment model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203153657) do
+ActiveRecord::Schema.define(version: 20170206084457) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string  "photo"
